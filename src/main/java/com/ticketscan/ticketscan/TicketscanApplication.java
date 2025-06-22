e32d0240e78cf90de7e2fa765b75e9455d961869
Kedarnath Peraka <kedarnathperaka@gmail.com>
2025-06-22 13:52:14 +0530

    initial commmit

---
+++ src/main/java/com/ticketscan/ticketscan/TicketscanApplication.java
@@ -0,0 +1,13 @@
+package com.ticketscan.ticketscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketscan/ticketscan/TicketscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketscan.ticketscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
